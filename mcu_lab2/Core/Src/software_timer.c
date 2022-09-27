047f375b5a9c14f1412f80e4ba9a2a8bf08b211e
Nguyen Phuc Tien <nguyenphuctien1258@gmail.com>
2022-09-27 12:43:40 +0700

    done but the delay doesnt low enough

--- mcu_lab2/Core/Src/software_timer.c
+++ mcu_lab2/Core/Src/software_timer.c
@@ -10,9 +10,11 @@
 int timer1_flag = 0;
 int timer2_flag = 0;
 int timer3_flag = 0;
+int timer4_flag = 0;
 int timer1_counter = 0;
 int timer2_counter = 0;
 int timer3_counter = 0;
+int timer4_counter = 0;
 int TIMER_CYCLE = 10;
 
 void setTimer1(int duration) {
@@ -27,6 +29,10 @@ void setTimer3(int duration) {
 	timer3_counter = duration;
 	timer3_flag = 0;
 }
+void setTimer4(int duration) {
+	timer4_counter = duration;
+	timer4_flag = 0;
+}
 void timerRun() {
 	if (timer1_counter > 0) {
 		timer1_counter--;
@@ -34,18 +40,22 @@ void timerRun() {
 			timer1_flag = 1;
 		}
 	}
-
 	if (timer2_counter > 0) {
 		timer2_counter--;
 		if (timer2_counter <= 0) {
 			timer2_flag = 1;
 		}
 	}
-
 	if (timer3_counter > 0) {
 		timer3_counter--;
 		if (timer3_counter <= 0) {
 			timer3_flag = 1;
+		}
+	}
+	if (timer4_counter > 0) {
+		timer4_counter--;
+		if (timer4_counter <= 0) {
+			timer4_flag = 1;
 		}
 	}
 }
